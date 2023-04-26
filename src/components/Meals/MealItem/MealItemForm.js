f3a61960bd319f5876d3914f119df4082a0735c0
kunlanis <yoo.kunlanis@gmail.com>
2023-04-26 16:25:35 +0700

    Adding a Meal Form

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import styles from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
 
 const MealItemForm = () => {
   return (
-    <div>MealItemForm</div>
-  )
-}
+    <form className={styles.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>Add</button>
+    </form>
+  );
+};
 
-export default MealItemForm
+export default MealItemForm;
